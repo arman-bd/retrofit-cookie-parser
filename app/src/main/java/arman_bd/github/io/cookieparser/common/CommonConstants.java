9d98c98bd28102c0c7b053fb35e8b779269eb79d
Arman Hossain <arman-bd@users.noreply.github.com>
2021-06-07 23:37:05 +0600

    Update CommonConstants.java

--- app/src/main/java/arman_bd/github/io/cookieparser/common/CommonConstants.java
+++ app/src/main/java/arman_bd/github/io/cookieparser/common/CommonConstants.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class CommonConstants {
 
-    public final static String API_BASE_URL = "https://agilex.smrobi.com/";
+    public final static String API_BASE_URL = "https://example.com/";
 
     public static String AUTH_COOKIE = "";
     public static String AUTH_TOKEN = "";
